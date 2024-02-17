eae52b9d635dd2a67b41c147b864b432ffc4302d
boluamoo <boluamoo58@gmail.com>
2024-02-17 22:15:04 +0100

    toggle-hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useToggle } from './components/useToggle'
 
 function App() {
+  const [isVisible, toggle] = useToggle()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggle}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isVisible && <h1>Hidden text</h1>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
